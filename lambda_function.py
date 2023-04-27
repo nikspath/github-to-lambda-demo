e1d38b6898e2e27036f2ec04e179e802e18b132f
nikhil <dropboxnikspath@gmail.com>
2023-04-27 12:48:13 +0530

    random number

--- lambda_function.py
+++ lambda_function.py
@@ -1,2 +1,19 @@
+import json
+import random
+import logging
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+def fetchdata():
+    data = ['1','2','3','4']
+    return random.choice(data)
+
+
 def lambda_handler(event, context):
-    print('Done x1.1')
+    logger.info("Event data: %s", event)
+    data=fetchdata()
+    message=f"my new number {data}"
+    response_dict = {'statusCode': 200,'body': json.dumps({"message":message})}
+    logger.info("Response data: %s", response_dict)
+    return response_dict
