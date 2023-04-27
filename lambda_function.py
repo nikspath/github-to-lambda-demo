127a6e612f9c12b700991f0d985946ebc4829229
nikspath <45727113+nikspath@users.noreply.github.com>
2023-04-27 13:28:04 +0530

    test

--- lambda_function.py
+++ lambda_function.py
@@ -13,7 +13,7 @@ def fetchdata():
 def lambda_handler(event, context):
     logger.info("Event data: %s", event)
     data=fetchdata()
-    message=f"my new number = {data}"
+    message=f"my new number {data}"
     response_dict = {'statusCode': 200,'body': json.dumps({"message":message})}
     logger.info("Response data: %s", response_dict)
     return response_dict
