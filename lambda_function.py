4bc9bf77db9dd9f13c27bdf494cc25b8bde31aa7
nikhil <dropboxnikspath@gmail.com>
2023-04-27 12:28:28 +0530

    add panda

--- lambda_function.py
+++ lambda_function.py
@@ -1,4 +1,8 @@
-def lambda_function():
-	print("nikhil 1.1")
-	return {"body":"test"}
+import pandas as pd
 
+def lambda_handler(event, context):
+    d = {'col1': [1,2], 'col2': [3,4]}
+    df = pd.DataFrame(data=d)
+    print(df)
+    print('Done x1.1')
+    
